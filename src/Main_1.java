a33549616eb9ef168fffe051077969eeb7acd952
ted7088 <ted3131@naver.com>
2023-08-03 13:48:49 +0900

    Initial commit

---
+++ src/Main_1.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main_1 {
+
+    static String answer = "";
+    static int n, total = 0;
+    boolean flag = false;
+
+    public void DFS(int L, int sum, int[] arr) {
+        if (flag) return;
+        if (L == n) {
+            if ((total - sum) == sum) {
+                answer = "YES";
+                flag = true;
+            }
+        } else {
+            DFS(L + 1, sum + arr[L], arr);
+            DFS(L + 1, sum, arr);
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+
+        Main_1 T = new Main_1();
+        Scanner kb = new Scanner(System.in);
+
+        n = kb.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+            total += arr[i];
+        }
+        T.DFS(0, 0, arr);
+        System.out.println(answer);
+    }
+}
