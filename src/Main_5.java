93fb421ef8b25b4e6d70d4cc16b480c43f8fffc5
ted7088 <ted3131@naver.com>
2023-08-16 11:27:15 +0900

    commit

---
+++ src/Main_5.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main_5 {
+    static int n, m;
+    static int answer = Integer.MAX_VALUE;
+
+    public void DFS(int L, int sum, int[] arr) {
+        if (sum > m) return;
+        if (L >= answer) return;
+        if (sum == m) {
+            answer = Math.min(answer, L);
+        } else {
+            for (int i = 0; i < n; i++) {
+                DFS(L + 1, sum + arr[i], arr);
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Main_5 T = new Main_5();
+
+        Scanner kb = new Scanner(System.in);
+        n = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = kb.nextInt();
+
+        m = kb.nextInt();
+        T.DFS(0, 0, arr);
+        System.out.println(answer);
+
+
+    }
+}
